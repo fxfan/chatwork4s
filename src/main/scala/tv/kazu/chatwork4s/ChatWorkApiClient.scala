e23f23767200f79eb190bc00a23cbdb0f41e4663
KASHIMA, Kazuo <k4200@kazu.tv>
2016-09-26 00:42:41 +0900

    Add fooOrErrors methods; Seq => List in return types

--- src/main/scala/tv/kazu/chatwork4s/ChatWorkApiClient.scala
+++ src/main/scala/tv/kazu/chatwork4s/ChatWorkApiClient.scala
@@ -18,41 +18,61 @@ trait ChatWorkApiClientBase {
 
   def me(): Future[UserFull] = {
     get("/me").map { response =>
-      response.jsValue.as[UserFull]
+      response.as[UserFull]
     }
   }
 
-  def contacts(): Future[Seq[User]] = {
+  def contacts(): Future[List[User]] = {
     get("/contacts").map { response =>
-      response.jsValue.as[Seq[User]]
+      response.as[List[User]]
     }
   }
 
   def room(roomId: Int): Future[Room] = {
+    roomOrErrors(roomId).map(throwLeft(_))
+  }
+
+  def roomOrErrors(roomId: Int): Future[Either[List[String], Room]] = {
     get("/rooms/" + roomId).map { response =>
-      response.jsValue.as[Room]
+      response.asEither[Room]
     }
   }
 
-  def rooms(): Future[Seq[Room]] = {
+  def rooms(): Future[List[Room]] = {
+    roomsOrErrors().map(throwLeft(_))
+  }
+
+  def roomsOrErrors(): Future[Either[List[String], List[Room]]] = {
     get("/rooms").map { response =>
-      response.jsValue.as[Seq[Room]]
+      response.asEither[List[Room]]
     }
   }
 
-  def roomMembers(roomId: Int): Future[Seq[User]] = {
+  def roomMembers(roomId: Int): Future[List[User]] = {
+    roomMembersOrErrors(roomId).map(throwLeft(_))
+  }
+
+  def roomMembersOrErrors(roomId: Int): Future[Either[List[String], List[User]]] = {
     get(s"/rooms/${roomId}/members").map { response =>
-      response.jsValue.as[Seq[User]]
+      response.asEither[List[User]]
     }
   }
 
-  def messages(roomId: Int, fetchNext: Boolean = false) = {
+  def messages(roomId: Int, fetchNext: Boolean = false): Future[List[Message]] = {
+    messagesOrErrors(roomId, fetchNext).map(throwLeft(_))
+  }
+
+  def messagesOrErrors(roomId: Int, fetchNext: Boolean = false): Future[Either[List[String], List[Message]]] = {
     val params = if (fetchNext) Seq() else Seq(("force", "1"))
     get(s"/rooms/${roomId}/messages", params).map { response =>
-      if (response.jsValue != JsNull) {
-        response.jsValue.as[Seq[Message]]
+      if (response.httpResponse.isSuccess) {
+        if (response.jsValue != JsNull) {
+          Right(response.as[List[Message]])
+        } else {
+          Right(Nil)
+        }
       } else {
-        Seq()
+        response.asEither[List[Message]] // This should return Left[List[String]]
       }
     }
   }
@@ -82,6 +102,14 @@ trait ChatWorkApiClientBase {
       throw new IllegalArgumentException(s"'path' must start with '/', but '$path' was passed.")
     }
     Http(apiBase + path).header("X-ChatWorkToken", apiToken).timeout(connTimeoutMs, readTimeoutMs)
+  }
+
+  private def throwLeft[T](either: Either[List[String], T]): T = {
+    either match {
+      case Left(list) =>
+        throw new ApiErrorException(list)
+      case Right(x) => x
+    }
   }
 }
 
